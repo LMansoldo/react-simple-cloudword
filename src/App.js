1ea9133b750db21ea39027a4f04338c8bc2c74b8
LMansoldo <lucasmansoldo0@gmail.com>
2021-08-18 13:07:10 -0300

    chore: add dependencies, lint & docker-compose

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <p>{`Hi ${process.env.REACT_APP_NAME}`} </p>
           Learn React
         </a>
       </header>
